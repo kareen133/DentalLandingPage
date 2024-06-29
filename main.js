f2a19826648b5164bc8b99b0540bba2eb292249c
kareen133 <kareen3alkhateeb3@gmail.com>
2024-06-29 22:12:28 +0300

    dental care landing page

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    const toggleMenu =document.querySelector(".toggleMenu");
+    const Navigation =document.querySelector(".Navigation");
+    toggleMenu.classList.toggle("active");
+    Navigation.classList.toggle("active");
+}
+
+
